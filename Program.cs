30a2d5440b1234e8b8797e2d32240119836df027
Попов Артём <artemp_2016@mail.ru>
2021-12-16 10:59:58 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение радиуса: ");
+            double r = double.Parse(Console.ReadLine());
+            double C = 2 * Math.PI * r;
+            double S = Math.PI * (r * r);
+            Console.WriteLine("C={0:F2}",C);
+            Console.WriteLine("S={0:F2}",S);
+            Console.WriteLine();
+        }
+    }
+}
